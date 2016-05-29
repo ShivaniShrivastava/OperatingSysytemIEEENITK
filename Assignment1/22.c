620a912f06697e19681c38cc47481a1b4c4a52d6
ShivaniShrivastava <shivanisri8124@gmail.com>
2016-05-29 22:49:56 +0530

    Create 22.c

---
+++ Assignment1/22.c
@@ -0,0 +1,11 @@
+//execl
+
+#include <stdio.h>
+#include<unistd.h>
+
+        int main()
+        {
+                execl("/bin/rm", "rm", "shivi.c", NULL);
+                printf("Can only get here on error\n");
+                return 0;
+        }
