be925d5f3f1686671f754c828cee36b7ccd85757
ShivaniShrivastava <shivanisri8124@gmail.com>
2016-05-29 22:53:32 +0530

    Create 33.c

---
+++ Assignment1/33.c
@@ -0,0 +1,15 @@
+//execvp
+
+#include<stdio.h>
+#include <unistd.h>
+
+       void  main()
+
+        {
+char *args[3];
+           args[0]="rm";
+           args[1]="shivi.c";
+        args[2]=NULL;
+execvp(args[0], args);
+
+        }
