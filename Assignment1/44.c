c3a401debd244463efcf5a069588d3702e97952d
ShivaniShrivastava <shivanisri8124@gmail.com>
2016-05-29 22:57:03 +0530

    Create 44.c

---
+++ Assignment1/44.c
@@ -0,0 +1,19 @@
+//read open
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+void main()
+{
+
+        char array[4000];
+        int h=open("/.shivi.c",O_RDONLY);
+        if(read(h,&array,3900)<0)
+        {
+        printf("Error reading the file");
+        }
+        }
+
